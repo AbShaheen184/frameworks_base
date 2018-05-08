4b56b8fa7d9eedc11164c8003604e9539543edd2
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 05:33:21 +0000

    Running services icon toggle [1/2]
    aex edits(@ishubhamsingh):- * Adapt for oreo
                                * Clean up useless stuffs
                                * Disabled by default
    
    Change-Id: Ic92795891f907110cc40d539a34918d09e2af156
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5283,6 +5283,12 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Whether to show or hide the running services icon
+         * @hide
+         */
+        public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
